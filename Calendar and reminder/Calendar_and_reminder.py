d6702786e0f9b8419bb70a040922452a191c430a
LyudmilLilov <lusililov@gmail.com>
2024-01-29 22:21:01 +0200

    Calendar_and_reminder.py

--- Calendar and reminder/Calendar_and_reminder.py
+++ Calendar and reminder/Calendar_and_reminder.py
@@ -20,7 +20,18 @@ def main():
 
         choice = input("Choose an option (1-3): ")
 
-
+        if choice == "1":
+            event_date = input("Enter the date (YYYY-MM-DD): ")
+            event_description = input("Enter event description: ")
+            my_calendar.add_event(event_date, event_description)
+        elif choice == "2":
+            event_date = input("Enter the date to view events (YYYY-MM-DD): ")
+            my_calendar.view_events(event_date)
+        elif choice == "3":
+            print("Exiting the calendar program. Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please enter a valid option.")
 
 if __name__ == "__main__":
     main()
