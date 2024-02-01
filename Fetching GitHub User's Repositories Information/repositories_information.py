e113f47a21af1ebedc6c6b0fa3f03233e1514943
LyudmilLilov <lusililov@gmail.com>
2024-02-01 22:47:09 +0200

    repositories_information.py

---
+++ Fetching GitHub User's Repositories Information/repositories_information.py
@@ -0,0 +1,17 @@
+import requests
+
+
+def fetch_user_repositories(username):
+    pass
+
+
+def display_repository_info(repositories):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
