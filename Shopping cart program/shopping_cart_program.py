05261de70e0b83944df6e7fb592aec28d51eddd9
LyudmilLilov <155525445+LyudmilLilov@users.noreply.github.com>
2024-04-12 23:15:52 +0300

    Create shopping_cart_program.py

---
+++ Shopping cart program/shopping_cart_program.py
@@ -0,0 +1,23 @@
+foods = []
+prices = []
+total = 0
+
+while True:
+    food = input("Enter a food to buy (exit to quit): ")
+    if food.lower() == "exit":
+        break
+    else:
+        price = float(input(f"Enter the price of a {food}: $"))
+        foods.append(food)
+        prices.append(price)
+
+print("------ YOUR CART ------")
+
+for food in foods:
+    print(food, end=" ")
+
+for price in prices:
+    total += price
+
+print()
+print(f"Your total is: ${total}")
